9577a4f0178e96f2338aef245f03785d90911d88
MichailoZaichenko <michailo.zaichenko@gmail.com>
2024-12-02 01:28:19 +0200

    fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,19 +2,19 @@
 
 // ====== USER PARAMS
 declare type CreateUserParams = {
-  clerkId: string;
-  email: string;
-  username: string;
+  clerkId: string?;
+  email: string?;
+  username: string?;
   firstName: string?;
   lastName: string?;
-  photo: string;
+  photo: string?;
 };
 
 declare type UpdateUserParams = {
   firstName: string?;
   lastName: string?;
-  username: string;
-  photo: string;
+  username: string?;
+  photo: string?;
 };
 
 // ====== IMAGE PARAMS
